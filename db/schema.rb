591a95e5bb47cd014c90c6507e2050f79506a41c
Glauber Marques <glauberfmarques@gmail.com>
2023-10-10 06:07:55 -0600

    Add first test configuration and rspec set up

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_10_03_115148) do
+ActiveRecord::Schema.define(version: 2023_10_03_132143) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
